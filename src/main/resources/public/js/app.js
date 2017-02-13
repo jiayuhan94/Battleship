322eb1bd607d414375f25525f0136a93feba9571
jiayuhan94 <hanjia@oregonstate.edu>
2017-02-13 09:07:35 -0800

    this is just scan part at back end and edit about front end

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -16,8 +16,31 @@ function cancelShips() {
     div = document.getElementById('place-ships');
     div.style.display = "none";
 }
-function error() {
-    alert("I'm an error message. I can be styled if you're interested in a small external framework. Check out SweetAlert");
+function scan() {
+    console.log($( "#rowFire" ).val());
+    console.log($( "#colFire" ).val());
+//var menuId = $( "ul.nav" ).first().attr( "id" );
+    var request = $.ajax({
+        url: "/scan/"+$( "#rowFire" ).val()+"/"+$( "#colFire" ).val(),
+        method: "post",
+        data: JSON.stringify(gameModel),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json"
+   });
+
+   request.done(function( currModel ) {
+     displayGameState(currModel);
+     gameModel = currModel;
+     return results;
+
+   });
+
+   request.fail(function( jqXHR, textStatus ) {
+     alert( "Request failed: " + textStatus );
+   });
+
+
+//    alert("I'm an error message. I can be styled if you're interested in a small external framework. Check out SweetAlert");
 }
 
 function placeShip() {
